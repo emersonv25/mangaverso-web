7b1c9b60e66619258dc4aff824cada4d1f267df7
emersonv25 <emersondejesussantos@hotmail.com>
2023-10-11 19:59:18 -0300

    imagem responsiva

--- src/components/MangaCard/MangaCard.tsx
+++ src/components/MangaCard/MangaCard.tsx
@@ -8,20 +8,23 @@ type MangaCardProps = {
 export const MangaCard: React.FC<MangaCardProps> = ({ manga }: MangaCardProps) => {
     return (
         <Link href={`/mangas/${manga.id}`}>
-            <div className='flex flex-col min-w-[250px] min-h-[400px]  max-w-[250px] max-h-[400px] overflow-hidden rounded-lg relative'>
-                <div className='absolute bg-black w-[250px] p-1 top-0 opacity-80 px-2 '>
-                    <p className='opacity-100 line-clamp-1 text-white font-bold'>{manga.title}</p>
-                </div>
+            <div className='flex flex-col min-w-[125px] min-h-[200px] max-w-[200px] overflow-hidden rounded-lg relative'>
                 <Image
                     src={manga.posterUrl}
                     alt={manga.title}
-                    width={250}
-                    height={400}
+                    layout="responsive"
+                    width={1000}
+                    height={600}
+                    sizes="(max-width: 600px) 100vw, 50vw"
+                    style={{ width: '100%', height: 'auto' }}
                 />
-
+                <div className='absolute bg-black w-full p-1 bottom-0 opacity-80'>
+                    <p className='opacity-100 line-clamp-2 text-white font-bold text-xs sm:text-sm md:text-base lg:text-lg text-center'>
+                        {manga.title}
+                    </p>
+                </div>
             </div>
         </Link>
-
 
     )
 }
