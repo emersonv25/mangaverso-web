a3af01bfc9028b6871924336ce425786775aed57
matheus55391 <matheus.manulo55@gmail.com>
2023-10-17 13:18:23 -0300

    fix/textStyle

--- src/components/MangaCard/index.tsx
+++ src/components/MangaCard/index.tsx
@@ -8,7 +8,7 @@ type MangaCardProps = {
 const MangaCard: React.FC<MangaCardProps> = ({ manga }: MangaCardProps) => {
     return (
         <Link href={`/mangas/${manga.id}`}>
-            <div className='flex flex-col min-w-[125px] min-h-[200px] max-w-[200px] overflow-hidden rounded-lg relative'>
+            <div className='flex flex-col min-w-[125px] min-h-[200px] max-w-[200px] overflow-hidden rounded-lg '>
                 <Image
                     src={manga.posterUrl}
                     alt={manga.title}
@@ -18,8 +18,8 @@ const MangaCard: React.FC<MangaCardProps> = ({ manga }: MangaCardProps) => {
                     sizes="(max-width: 600px) 100vw, 50vw"
                     style={{ width: '100%', height: 'auto' }}
                 />
-                <div className='absolute bg-black w-full p-1 bottom-0 opacity-80'>
-                    <p className='opacity-100 line-clamp-2 text-white font-bold text-xs sm:text-sm md:text-base lg:text-lg text-center'>
+                <div className=' bg-black w-full p-1 bottom-0 opacity-80'>
+                    <p className='opacity-100 line-clamp-1 hover:line-clamp-2 text-white font-bold text-xs sm:text-xs lg:text-sm text-center'>
                         {manga.title}
                     </p>
                 </div>
