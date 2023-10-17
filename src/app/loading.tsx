dee7dc970dd89cf1ff78b4a3b656ca1a0974097e
matheus55391 <matheus.manulo55@gmail.com>
2023-10-17 19:01:02 -0300

    fix/ssrHomePage

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return(
         <div className="flex flex-col min-h-screen bg-zinc-700 p-4">
-            <h1 className="text-white">Loading</h1>
+            <h1 className="text-white">Loading...</h1>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import MangaCard from '@/components/MangaCard';
 import { IManga } from '@/interfaces/IManga';
 
@@ -11,25 +10,31 @@ interface MangaResponse {
 
 const getData = async () => {
   const res = await fetch('https://tiny-fawn-shawl.cyclic.app/api/v1/manga')
-  if (res.status == 200) {
+  if (res.status === 200) {
     const data = await res.json() as MangaResponse
     return data
-  } else {
-    throw new Error('Error ao carregar os mangas!')
   }
+  return new Promise<MangaResponse>((resolve, reject) => {
+    resolve({
+      currentPage: 1,
+      data: [],
+      totalCount: 0,
+      totalPages: 0
+    })
+  })
 }
 
 export default async function Home() {
-  const data = await getData()
+  const mangas = await getData()
 
   return (
     <div className="flex flex-col min-h-screen bg-zinc-700 p-4">
       <div className='flex flex-col'>
         <h1 className='text-white font-bold' >Os mais lidos no momento!</h1>
 
-        <div className='flex flex-row overflow-x-scroll space-x-3 scrollbar scrollbar-thin scrollbar-thumb-gray-900 scrollbar-thumb-gray-500 p-4'>
+        <div className='flex flex-row overflow-x-scroll space-x-3 scrollbar-thin scrollbar-thumb-gray-900  p-4'>
           {
-            data?.data.map((manga, index) => {
+            mangas?.data.map((manga, index) => {
               return (
                 <MangaCard manga={manga} key={index} />
               )
