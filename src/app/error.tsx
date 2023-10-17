3ff3d94e0fb02475ee023c4d52fd2d8a77a58367
matheus55391 <matheus.manulo55@gmail.com>
2023-10-17 20:07:35 -0300

    newErrorScreen

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,30 +1,31 @@
-'use client' // Error components must be Client Components
-
-import { useEffect } from 'react'
+'use client';
+import { useEffect } from 'react';
 
 export default function Error({
     error,
     reset,
 }: {
-    error: Error & { digest?: string }
-    reset: () => void
+    error: Error & { digest?: string };
+    reset: () => void;
 }) {
     useEffect(() => {
         // Log the error to an error reporting service
-        console.error(error)
-    }, [error])
+        console.error(error);
+    }, [error]);
 
     return (
-        <div>
-            <h2>Something went wrong!</h2>
+        <div className="min-h-screen flex flex-col items-center justify-center ">
+            <h2 className="text-3xl font-semibold mb-4">
+                Ops, algo deu errado!
+            </h2>
+            <p className="mb-4">
+                Parece que encontramos um erro inesperado. Por favor, tente novamente.
+            </p>
             <button
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
-            >
-                Try again
+                onClick={reset}
+                className="bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 rounded-full">
+                Tente novamente
             </button>
         </div>
-    )
+    );
 }
